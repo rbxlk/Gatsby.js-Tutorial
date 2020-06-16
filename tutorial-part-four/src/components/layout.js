100ccd9b8d7f7590de197dd3a9f9d0ce9620f8ce
rbxlk <rbxlk@protonmail.com>
2020-06-16 11:19:03 +0100

    Add title meta data to gatsby-file config file

--- tutorial-part-four/src/components/layout.js
+++ tutorial-part-four/src/components/layout.js
@@ -2,38 +2,38 @@ import React from "react"
 import { css } from "@emotion/core"
 import { Link } from "gatsby"
 
-import rhythm from "../utils/typography"
+import { rhythm } from "../utils/typography"
 
 export default function Layout({ children }) {
-    return (
-        <div
-            css={css`
-            margin: 0 auto;
-            max-width: 700px;
-            padding: ${rhythm(2)};
-            padding-top: ${rhythm(1.5)};
-            `}
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <Link to={`/`}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
         >
-            <Link to={`/`}>
-                <h3
-                    css={css`
-                        margin-bottom: ${rhythm(2)};
-                        display: inline-block;
-                        font-style: normal;
-                    `}
-                >
-                    Pandas Eating Lots
-                </h3>
-            </Link>
-            <Link
-                to={`/about/`}
-                css={css`
-                    float: right;
-                `}
-            >
-                About
-            </Link>
-            {children}
-        </div>
-    )
+          Pandas Eating Lots
+        </h3>
+      </Link>
+      <Link
+        to={`/about/`}
+        css={css`
+          float: right;
+        `}
+      >
+        About
+      </Link>
+      {children}
+    </div>
+  )
 }
